d9a8fda4d417cb3bf398749d204cde0ca09a0081
foxy <69487888+foxyyx@users.noreply.github.com>
2023-06-24 13:19:44 -0300

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,6 @@
+iprint (convertBytes (10))
+iprint (convertBytes (1000))
+iprint (convertBytes (10000000))
+iprint (convertBytes (100000000))
+iprint (convertBytes (1000000000))
+iprint (convertBytes (10000000000))
