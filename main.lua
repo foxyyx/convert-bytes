3d151ebeafeb7a4990e96f9940be7bdfba981637
fxy <69487888+foxyyx@users.noreply.github.com>
2024-02-09 13:56:05 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
+
+local math_floor = math.floor
+
 function convertBytes (bytes)
     if (not bytes) then return error ('Error in argument #1. Set the number of bytes') end
-    local bytes = math.floor (bytes)
-    local kbytes, mbytes, gbytes = bytes / 1000, bytes / 1000000, bytes / 1000000000
+    local bytes = bytes
+    local kbytes, mbytes, gbytes = math_floor (bytes / 1000), math_floor (bytes / 1000000), math_floor (bytes / 1000000000)
     return ((bytes > 0 and bytes < 1000 and bytes.. ' B') or ((kbytes > 0 and kbytes < 1000) and kbytes.. ' KB') or ((mbytes > 0 and mbytes < 1000) and mbytes.. ' MB') or ((gbytes > 0 and gbytes < 1000) and gbytes.. ' GB'))
 end
 
@@ -12,5 +15,5 @@ function getFileSize (path)
        bytes = fileGetSize (file)
     end
     fileClose (file)
-    return convertBytes (bytes)
+    return bytes
 end
